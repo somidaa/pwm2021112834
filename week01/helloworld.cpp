290ba0d333cb514cd96b7b4bcced4a09dfc93e58
somida <3012175227@qq.com>
2022-07-17 09:41:43 +0000

    add helloword. For finish my work,I need write a code and submit it.

---
+++ week01/helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+/**
+ * @brief 
+ * @return int 
+ * @author panwenmei (2021112834.stu.hit.edu.cn)
+ */
+int main()
+ {
+    std::cout << "Hello World!" << std::endl;
+    getchar();
+    return 0;
+}
